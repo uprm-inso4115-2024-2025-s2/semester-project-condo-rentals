32c9b7067f84b16b5a41acddd31f57ef36bcb2fe
William A. Martinez Martinez <william.martinez10@upr.edu>
2025-02-19 10:41:56 -0400

    Added listing preview card to listings tab

--- condo-rentals/app/(tabs)/Listings.tsx
+++ condo-rentals/app/(tabs)/Listings.tsx
@@ -1,14 +1,49 @@
-import { Text, View } from "react-native";
-export default function Listings() {
+import React from "react";
+import { View, ScrollView, StyleSheet } from "react-native";
+import RentalListingPreviewCard from "../../components/ListingsPreview";
+
+const Listings = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Rental Listings & Search Team</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.grid}>
+        <RentalListingPreviewCard
+          landlordName="John Doe"
+          priceLocation="$400 - Pueblo Mayagüez"
+          onPress={() => console.log("Navigating to details")}
+        />
+        {/* <RentalListingCard
+          landlordName="Ana López"
+          priceLocation="$950 - Condominio El Escorial"
+          onPress={() => console.log("Navigating to details")}
+        />
+        <RentalListingCard
+          landlordName="Carlos Rivera"
+          priceLocation="$500 - Condominio Alturas de Mayagüez"
+          onPress={() => console.log("Navigating to details")}
+        />
+        <RentalListingCard
+          landlordName="Roberto Torres"
+          priceLocation="$500 - Mayagüez Terrace"
+          onPress={() => console.log("Navigating to details")}
+        /> */}
+      </View>
+    </ScrollView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    alignItems: "center", 
+    justifyContent: "center", 
+    paddingVertical: 20,
+  },
+  grid: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center", 
+    gap: 15, 
+  },
+});
+
+export default Listings;
