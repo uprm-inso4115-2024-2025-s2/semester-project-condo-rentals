8e777d4728724c7df9d7fe6aefadd72d2ea74d2e
Yahid1 <yahid.diaz@upr.edu>
2025-03-06 19:33:34 -0400

    Added example Marker

--- condo-rentals/app/(tabs)/Map.tsx
+++ condo-rentals/app/(tabs)/Map.tsx
@@ -2,8 +2,21 @@ import { Text, View } from "react-native";
 import MapView from 'react-native-maps';
 import {
   StyleSheet} from "react-native"
+import CondoMarker from "@/components/CondoMarker";
+import { CondoMarkerProps } from "@/components/InterfaceMarker";
 
 export default function Map() {
+
+  let Example: CondoMarkerProps = {
+    id: 1,
+    name: "Condo Name",
+    description: "Condo Description",
+    location: {
+      latitude: 18.2106,
+      longitude: -67.1396,
+    }
+  }
+
   return (
     
     <View
@@ -16,20 +29,26 @@ export default function Map() {
 
 
 
-<MapView 
-  style={styles.map} 
-  // showsMyLocationButton={true}
-  showsUserLocation={true}
-  
-  // Initial Location set to UPRM
-  // TODO: Update to user location always 
-  initialRegion={{
-    latitude: 18.2106,
-    longitude: -67.1396,
-    latitudeDelta: 0.009,
-    longitudeDelta: 0.009,
-  }}
-/>
+      <MapView 
+        style={styles.map} 
+        // showsMyLocationButton={true}
+        showsUserLocation={true}
+        
+        // Initial Location set to UPRM
+        // TODO: Update to user location always 
+        initialRegion={{
+          latitude: 18.2106,
+          longitude: -67.1396,
+          latitudeDelta: 0.009,
+          longitudeDelta: 0.009,
+        }}
+      >
+        <CondoMarker id={Example.id} name={Example.name} description={Example.description} location={Example.location}/>
+        
+        
+
+        
+      </MapView>
 
 
     </View>
