13f6bf46bc792a7fac435f214ed50f4e72809bf9
jonVargas1 <jon.vargas@upr.edu>
2025-03-08 19:30:09 -0400

    added Locate User button design

--- condo-rentals/app/(tabs)/Map.tsx
+++ condo-rentals/app/(tabs)/Map.tsx
@@ -1,7 +1,8 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View, Image} from "react-native";
 import MapView from 'react-native-maps';
 import {
   StyleSheet} from "react-native"
+
 
 export default function Map() {
   return (
@@ -14,10 +15,14 @@ export default function Map() {
       }}
     >
 
+  <MapView style={styles.map} />
 
-
-<MapView style={styles.map} />
-
+  <TouchableOpacity style={styles.centerbutton}>
+    <Image 
+    source={require("../../assets/images/Condo Rental Assets/Condo_UserLocation.png")}
+    style={styles.buttonImage} 
+    />
+  </TouchableOpacity>
 
     </View>
   );
@@ -28,5 +33,21 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "80%",
 
+  },
+  centerbutton: {
+    position: "absolute",
+    bottom: "12%",
+    right: 20,
+    backgroundColor: "white",
+    width: 45,
+    height: 45,
+    borderRadius: 45/2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonImage: {
+    width:35,
+    height: 35,
+    resizeMode: "contain",
   },
 });
