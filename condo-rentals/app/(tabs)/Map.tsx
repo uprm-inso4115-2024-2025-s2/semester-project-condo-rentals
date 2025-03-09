891b71775c4032209c1a973f83c77c5e867bdae0
Jose-Irizarry <jose_irizarry@techexchange.in>
2025-03-08 22:08:01 -0400

    Added permissions for location

--- condo-rentals/app/(tabs)/Map.tsx
+++ condo-rentals/app/(tabs)/Map.tsx
@@ -1,9 +1,38 @@
 import { Text, View } from "react-native";
 import MapView from 'react-native-maps';
-import {
-  StyleSheet} from "react-native"
+import {StyleSheet} from "react-native"
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+
 
 export default function Map() {
+
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  let text = 'Waiting...';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     
     <View
@@ -40,6 +69,5 @@ const styles = StyleSheet.create({
   map: {
     width: "100%",
     height: "80%",
-
   },
 });
